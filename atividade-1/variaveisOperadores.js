8b78895ac1292d7f0a20c7e5e484443ae5bf8adf
Vinicius Lacerda <viniciuslacerdasoares@gmail.com>
2025-03-05 13:12:00 -0300

    adicionado:   atividade-1/variaveisOperadores.js

---
+++ atividade-1/variaveisOperadores.js
@@ -0,0 +1,25 @@
+//soma
+let n1Soma=5
+let n2Soma=2
+let Soma=0
+
+//subtração
+let n1Sub=4
+let n2Sub=1
+let Sub=0
+
+//multiplicação
+let n1Mult=7
+let n2Mult=3
+let Mult=0
+
+//divisão
+let n1Div=10
+let n2Div=2
+let Div=0
+
+//prints
+console.log('Resultado da Soma entre ${n1Soma} e ${n2Soma} = ${Soma=n1Soma+n2Soma}')
+console.log('Resultado da Subtração entre ${n1Sub} e ${n2Sub} = ${Sub=n1Sub+n2Sub}')
+console.log('Resultado da Multiplicação entre ${n1Mult} e ${n2Mult} = ${Mult=n1Mult+n2Mult}')
+console.log('Resultado da Divisão entre ${n1Div} e ${n2Div} = ${Div=n1Div+n2Div}')
